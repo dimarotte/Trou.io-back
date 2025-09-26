270b59caf90ed59446b9b41d0c7a14dc12f67ab9
anbourdier2 <boubix63@gmail.com>
2025-09-26 09:45:15 +0200

    creation room

--- src/rooms/MyRoom.ts
+++ src/rooms/MyRoom.ts
@@ -2,23 +2,32 @@ import { Room, Client } from "@colyseus/core";
 import { MyRoomState } from "./schema/MyRoomState";
 
 export class MyRoom extends Room<MyRoomState> {
-  maxClients = 4;
-  state = new MyRoomState();
-
+  // maxClients = 4;
+  // state = new MyRoomState();
+  
   onCreate (options: any) {
-    this.onMessage("type", (client, message) => {
-      //
-      // handle "type" message
-      //
+    this.setState(new MyRoomState());
+
+    this.onMessage("move", (client, message) => {
+      const player = this.state.players.get(client.sessionId); 
+      if (player) {
+        player.x += data.x;
+        player.y += data.y;
+      }
     });
+
   }
 
   onJoin (client: Client, options: any) {
     console.log(client.sessionId, "joined!");
+    const newPlayer = new Player();
+    newPlayer.name = options.name || "Guest";
+    this.state.players.set(client.sessionId, newPlayer);
   }
 
   onLeave (client: Client, consented: boolean) {
     console.log(client.sessionId, "left!");
+    this.state.players.delete(client.sessionId);
   }
 
   onDispose() {
