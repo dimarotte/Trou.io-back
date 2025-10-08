a631d212d17ee27dc855f4b3f95c740043d602d1
Jules Girard <julesgirard@laposte.net>
2025-10-08 11:33:12 +0200

    fix: correct type declaration for 'etat' in MyRoomState

--- src/rooms/schema/MyRoomState.ts
+++ src/rooms/schema/MyRoomState.ts
@@ -36,5 +36,5 @@ export class MyRoomState extends Schema {
   @type({ map: Player }) players = new MapSchema<Player>();
   @type({ map: Item }) items = new MapSchema<Item>();
   @type(Map) map = new Map();
-  @type(String) etat : String = "waiting"; 
+  @type("string") etat: string = "waiting"; 
 }
